51febc09145fb2141f094a01b777fec58fc5d222
Oceanzouyang <32188757+Oceanzouyang@users.noreply.github.com>
2017-09-22 18:55:06 +0800

    Add files via upload

---
+++ device.sql
@@ -0,0 +1,12 @@
+﻿DROP TABLE IF EXISTS device;
+CREATE TABLE public.device
+(
+  id serial,
+  name character varying(20),
+  type character varying(10),
+  status integer,
+  CONSTRAINT device_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO public.device(name, type, status)VALUES ('邹洋','A类', 1);
+INSERT INTO public.device(name, type, status)VALUES ('机器','B类', 1);
